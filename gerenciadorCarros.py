501d0d340f60ad0d22e61a3565b48651ff84d57e
Arthur <arthur.sousaoliveira@ufrpe.br>
2024-02-20 17:55:49 -0300

    "APRESENTAÇÃO"

--- gerenciadorCarros.py
+++ gerenciadorCarros.py
@@ -6,15 +6,16 @@ def main():
     carros = carregar_carros(arquivo_carros)
 
     while True:
-        print("\n1. Adicionar carro")
+        print("\nBem vindo ao gerenciador do estacionamento!")
+        print("1. Adicionar carro")
         print("2. Excluir carro")
         print("3. Editar carro")
         print("4. Mostrar carros")
         print("5. Sair")
 
-        escolha = input("\nEscolha uma opção: ")
+        escolha = int(input("\nEscolha uma opção: "))
 
-        if escolha == "1":
+        if escolha == 1:
             marca = input("Marca do carro: ").capitalize()
             modelo = input("Modelo do carro: ").capitalize()
             ano = int(input("Ano do carro: "))
@@ -24,12 +25,12 @@ def main():
             adicionar_carro(carros, novo_carro)
             salvar_carros(carros, arquivo_carros)
             print("Carro adicionado com sucesso.")
-        elif escolha == "2":
+        elif escolha == 2:
             indice = int(input("Índice do carro a ser excluído: "))
             excluir_carro(carros, indice - 1)
             salvar_carros(carros, arquivo_carros)
             print("Carro excluído com sucesso.")
-        elif escolha == "3":
+        elif escolha == 3:
             indice = int(input("Índice do carro a ser editado: "))
             marca = input("Nova marca do carro: ").capitalize()
             modelo = input("Novo modelo do carro: ").capitalize()
@@ -40,11 +41,11 @@ def main():
             editar_carro(carros, indice - 1, novo_carro)
             salvar_carros(carros, arquivo_carros)
             print("Carro editado com sucesso.") 
-        elif escolha == "4":
+        elif escolha == 4:
             print("\nCarros:")
             for i, carro in enumerate(carros):
                 print(f"{i + 1}. {carro['marca']} {carro['modelo']} ({carro['ano']}) - Vaga: {carro['vaga']}, Tipo: {carro['tipo_vaga']}")
-        elif escolha == "5":
+        elif escolha == 5:
             print("Saindo...")
             break
         else:
